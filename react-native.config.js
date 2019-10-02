f0ff6c88fe7de29d47386c43449bd64c33fe0f47
jorgemarrero <jorgems93@gmail.com>
2019-10-02 10:39:19 +0100

    fix: explain to auto-link exact routes

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      android: {
+        packageImportPath: 'import com.documentscanner.DocumentScannerPackage;',
+        packageInstance: 'new DocumentScannerPackage()'
+      }
+    }
+  }
+}
